77bc963a5d02a5c765b29694f53781bb7edc707a
Yes7933 <162532903+Yes7933@users.noreply.github.com>
2024-04-23 03:13:42 +0000

    done

---
+++ javascrip.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    var containers = document.getElementsByClassName('gamecontainer');
+    Array.from(containers).forEach(element => {
+        element.addEventListener('mouseover', () => {
+            element.classList.remove('reversehoverclass');
+            element.classList.add('hoverclass');
+        });
+        element.addEventListener('mouseout', () => {
+            element.classList.remove('hoverclass')
+            element.classList.add('reversehoverclass');
+        });
+    });
+});
